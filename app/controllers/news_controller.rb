dfdafe4d717885c97ce63e1370dc7d0392432c5c
ED1FF <edifei13@yandex.ua>
2018-03-09 12:02:44 +0200

    added defs for all news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,20 +1,67 @@
 require 'open-uri'
-require 'JSON'
+# require 'JSON'
 
 class NewsController < ApplicationController
-  def index
-    url = 'https://newsapi.org/v2/top-headlines?'\
-          'country=us&'\
-          'apiKey=6dd59350f84a476490213e76baf5d95b'
+
+  def open_url(url) #open JSON and conver it to array
     req = open(url)
     @response_body = req.read
     a = JSON.parse(@response_body)
     @b = a.to_a
-    # c = @b[2][1][0].to_json
-    # @z = JSON.parse(c)
-
-    # render json: @response_body.status
-    # render json: @response_body
-    # render plain: @z['title']
   end
+
+  def index #test
+    url = 'https://newsapi.org/v2/top-headlines?'\
+          'country=us&'\
+          "category=business&"\
+          'apiKey=6dd59350f84a476490213e76baf5d95b'
+    open_url(url)
+  end
+
+  def bbc #take top 20 news from BBC
+    url = 'https://newsapi.org/v2/top-headlines?'\
+          'sources=bbc-news&'\
+          'apiKey=6dd59350f84a476490213e76baf5d95b'
+    open_url(url)
+  end
+
+  def techcrunch #take top 20 news from techcrunch
+    url = 'https://newsapi.org/v2/top-headlines?'\
+          'sources=techcrunch&'\
+          'apiKey=6dd59350f84a476490213e76baf5d95b'
+    open_url(url)
+  end
+
+  def us(param) #us news with params
+    url = 'https://newsapi.org/v2/top-headlines?'\
+          'country=us&'\
+          "category=#{param}&"\
+          'apiKey=6dd59350f84a476490213e76baf5d95b'
+    open_url(url)
+  end
+
+  def ua(param) #ua news with params
+    url = 'https://newsapi.org/v2/top-headlines?'\
+          'country=ua&'\
+          "category=#{param}&"\
+          'apiKey=6dd59350f84a476490213e76baf5d95b'
+    open_url(url)
+  end
+
+  def ru(param) #ru news with params
+    url = 'https://newsapi.org/v2/top-headlines?'\
+          'country=ru&'\
+          "category=#{param}&"\
+          'apiKey=6dd59350f84a476490213e76baf5d95b'
+    open_url(url)
+  end
+
+  def uk(param) #uk news with params
+    url = 'https://newsapi.org/v2/top-headlines?'\
+          'country=uk&'\
+          "category=#{param}&"\
+          'apiKey=6dd59350f84a476490213e76baf5d95b'
+    open_url(url)
+  end
+
 end
