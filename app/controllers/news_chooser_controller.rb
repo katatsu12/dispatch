73595ebd2d98188d2c51fdcf8d2e3a3427906a27
ED1FF <edifei13@yandex.ua>
2018-03-19 22:30:21 +0200

    some code replaced to corect place

--- app/controllers/news_chooser_controller.rb
+++ app/controllers/news_chooser_controller.rb
@@ -1,15 +1,13 @@
 class NewsChooserController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user! , :set_current_user
 
+  def set_current_user
+    NewsChooser.current_user = current_user.id
+  end
 
   def index
-    if NewsChooser.where(user_id: current_user).count == 0
-      NewsChooser.create(:news_types => '0'*14, :user_id => current_user.id).save
-      index_info
-    else
-      index_info
-    end
-
+    NewsChooser.before_index
+    index_info
   end
 
   def send_email
@@ -20,12 +18,12 @@ class NewsChooserController < ApplicationController
 
   def bbc
     news_id = 0
-    news_types_changer(news_id)
+    NewsChooser.news_types_changer(news_id)
   end
 
   def tc
     news_id = 1
-    news_types_changer(news_id)
+    NewsChooser.news_types_changer(news_id)
   end
 
 
@@ -33,83 +31,67 @@ class NewsChooserController < ApplicationController
 
   def us_business
     news_id = 2
-    news_types_changer(news_id)
+    NewsChooser.news_types_changer(news_id)
   end
 
   def us_entertainment
     news_id = 3
-    news_types_changer(news_id)
+    NewsChooser.news_types_changer(news_id)
   end
 
   def us_health
     news_id = 4
-    news_types_changer(news_id)
+    NewsChooser.news_types_changer(news_id)
   end
 
   def us_science
     news_id = 5
-    news_types_changer(news_id)
+    NewsChooser.news_types_changer(news_id)
   end
 
   def us_sports
     news_id = 6
-    news_types_changer(news_id)
+    NewsChooser.news_types_changer(news_id)
   end
 
   def us_technology
     news_id = 7
-    news_types_changer(news_id)
+    NewsChooser.news_types_changer(news_id)
   end
 
   #ua--------------------------------------------------------------------------
 
   def ua_business
     news_id = 8
-    news_types_changer(news_id)
+    NewsChooser.news_types_changer(news_id)
   end
 
   def ua_entertainment
     news_id = 9
-    news_types_changer(news_id)
+    NewsChooser.news_types_changer(news_id)
   end
 
   def ua_health
     news_id = 10
-    news_types_changer(news_id)
+    NewsChooser.news_types_changer(news_id)
   end
 
   def ua_science
     news_id = 11
-    news_types_changer(news_id)
+    NewsChooser.news_types_changer(news_id)
   end
 
   def ua_sports
     news_id = 12
-    news_types_changer(news_id)
+    NewsChooser.news_types_changer(news_id)
   end
 
   def ua_technology
     news_id = 13
-    news_types_changer(news_id)
+    NewsChooser.news_types_changer(news_id)
   end
 
 private
-
-  def news_types_changer(news_id)
-    @news_chooser = NewsChooser.where(user_id: current_user).last
-    a = @news_chooser.news_types.each_char.to_a
-
-    if a[news_id] == '0'
-      a[news_id] = '1'
-    else
-      a[news_id] = '0'
-    end
-
-    b = a.join
-    @news_chooser.update!(:news_types =>  b)
-    redirect_to root_url
-  end
-
   def index_info
     @news_chooser = NewsChooser.where(user_id: current_user).last
     @a = @news_chooser.news_types.each_char.to_a
--- app/models/news_chooser.rb
+++ app/models/news_chooser.rb
@@ -1,3 +1,27 @@
 class NewsChooser < ApplicationRecord
+  cattr_accessor :current_user
   belongs_to :user
+
+  def self.news_types_changer(news_id)
+    @news_chooser = NewsChooser.where(user_id: current_user).last
+    a = @news_chooser.news_types.each_char.to_a
+
+    if a[news_id] == '0'
+      a[news_id] = '1'
+    else
+      a[news_id] = '0'
+    end
+
+    b = a.join
+    @news_chooser.update!(:news_types =>  b)
+  end
+
+  def self.before_index
+    if NewsChooser.where(user_id:  current_user).count == 0
+         NewsChooser.create(:news_types => '0'*14, :user_id => current_user).save
+    end
+  end
+
+
+
 end
