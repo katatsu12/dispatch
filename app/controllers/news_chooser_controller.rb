88065376a7f13c6692bf5bf72b798126968151d7
Den Zubritskiy <katatsu12@gmail.com>
2018-03-21 20:27:26 +0200

    New method swap

--- app/controllers/news_chooser_controller.rb
+++ app/controllers/news_chooser_controller.rb
@@ -1,5 +1,5 @@
 class NewsChooserController < ApplicationController
-  before_action :authenticate_user! , :set_current_user
+  before_action :authenticate_user!, :set_current_user
 
   def set_current_user
     NewsChooser.current_user = current_user.id
@@ -14,7 +14,7 @@ class NewsChooserController < ApplicationController
     NewsMailer.index(current_user).deliver
   end
 
-  #sites-----------------------------------------------------------------------
+  # sites-----------------------------------------------------------------------
   #
   # def bbc
   #   news_id = 0
@@ -27,84 +27,90 @@ class NewsChooserController < ApplicationController
   # end
   #
 
-  #us--------------------------------------------------------------------------
+  # us--------------------------------------------------------------------------
 
   def us_business
     news_id = 0
-    NewsChooser.news_types_changer(news_id)
+    swap(news_id)
   end
 
   def us_entertainment
     news_id = 1
-    NewsChooser.news_types_changer(news_id)
+    swap(news_id)
   end
 
   def us_health
     news_id = 2
-    NewsChooser.news_types_changer(news_id)
+    swap(news_id)
   end
 
   def us_science
     news_id = 3
-    NewsChooser.news_types_changer(news_id)
+    swap(news_id)
   end
 
   def us_sports
     news_id = 4
-    NewsChooser.news_types_changer(news_id)
+    swap(news_id)
   end
 
   def us_technology
     news_id = 5
-    NewsChooser.news_types_changer(news_id)
+    swap(news_id)
   end
 
-  #ua--------------------------------------------------------------------------
+  # ua--------------------------------------------------------------------------
 
   def ua_business
     news_id = 6
-    NewsChooser.news_types_changer(news_id)
+    swap(news_id)
   end
 
   def ua_entertainment
     news_id = 7
-    NewsChooser.news_types_changer(news_id)
+    swap(news_id)
   end
 
   def ua_health
     news_id = 8
-    NewsChooser.news_types_changer(news_id)
+    swap(news_id)
   end
 
   def ua_science
     news_id = 9
-    NewsChooser.news_types_changer(news_id)
+    swap(news_id)
   end
 
   def ua_sports
     news_id = 10
-    NewsChooser.news_types_changer(news_id)
+    swap(news_id)
   end
 
   def ua_technology
     news_id = 11
-    NewsChooser.news_types_changer(news_id)
+    swap(news_id)
   end
 
-private
+  private
+
   def index_info
     news_chooser = NewsChooser.where(user_id: current_user).last
     @news_types = news_chooser.news_types.each_char.to_a
     @index = 0
     if Notification.where(user_id: current_user).count == 0
-      @mail = "currently you dont have emaill. Please change it below"
+      @mail = 'currently you dont have emaill. Please change it below'
     else
       notification = Notification.where(user_id: current_user).last
       @mail = "your current emails is #{notification.email}"
     end
 
-    @news_labels = ['us_business', 'us_entertainment', 'us_health' \
-              , 'us_science', 'us_sports', 'us_technology', 'ua_business' \
-              , 'ua_entertainment', 'ua_health', 'ua_science' , 'ua_sports' , 'ua_technology']
+    @news_labels = %w[us_business us_entertainment us_health
+                      us_science us_sports us_technology ua_business
+                      ua_entertainment ua_health ua_science ua_sports ua_technology]
+  end
+
+  def swap(news_id)
+    NewsChooser.news_types_changer(news_id)
+    redirect_to root_path
   end
 end
