8615c7e8341a8c1a769d4519044d79d6120767bf
Den Zubritskiy <katatsu12@gmail.com>
2018-03-31 20:05:59 +0300

    Conflicts fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20180327153820) do
-=======
 ActiveRecord::Schema.define(version: 20180329160407) do
->>>>>>> sidekiq_news2
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
