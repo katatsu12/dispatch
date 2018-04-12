473b9fee9ef8eb2239e5f7996c4441ce7d2071d8
Den Zubritskiy <katatsu12@gmail.com>
2018-04-12 20:24:11 +0300

    Callback controller setup

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -12,7 +12,18 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
-  def twitter; end
+  def twitter
+    auth = request.env['omniauth.auth']
+    @user = User.where(provider: auth.provider, uid: auth.uid).first_or_create
+    if @user.persisted?
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Twitter'
+      sign_in_and_redirect @user, event: :authentication
+    else
+      @form = Twitter.new
+      session['devise.twitter_data'] = request.env['omniauth.auth'].except('extra')
+      render layout: 'none'
+    end
+  end
 
   def github
     user = User.from_omniauth(env['omniauth.auth'])
