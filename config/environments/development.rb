cad240feb5d670fd85327ac343cf7eda4707fb28
Den Zubritskiy <katatsu12@gmail.com>
2018-04-25 23:11:21 +0300

    Smtp setup, configure view for news delivery

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,15 +28,17 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :letter_opener
-  # config.action_mailer.smtp_settings = {
-  #   address:              'smtp.gmail.com',
-  #   port:                 587,
-  #   domain:               'localhost:3000',
-  #   user_name:            'dispatch.rails@gmail.com',
-  #   password:             'dispatchpass1234',
-  #   authentication:       'plain',
-  #   enable_starttls_auto: true  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'localhost:3000',
+    user_name:            Rails.application.secrets.gmail_username,
+    password:             Rails.application.secrets.gmail_password,
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.perform_caching = false
--- db/migrate/20180423145159_create_statistics.rb
+++ db/migrate/20180423145159_create_statistics.rb
@@ -19,6 +19,6 @@ class CreateStatistics < ActiveRecord::Migration[5.1]
     Statistic.create(news_type:"ua_science" , sended_times:"0" , current_subs: "0")
     Statistic.create(news_type:"ua_sports" , sended_times:"0" , current_subs: "0")
     Statistic.create(news_type:"ua_technology" , sended_times:"0" , current_subs: "0")
-    AdminUser.create(email: "admin@admin.admin" , password: "admin")
+    AdminUser.create(email: "admin@admin.com" , password: "admin")
   end
 end
