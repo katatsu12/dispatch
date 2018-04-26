2b2a4e7c2a8e0c187bdb9597dcf8675e808cb2d7
Den Zubritskiy <katatsu12@gmail.com>
2018-04-26 20:35:57 +0300

    Design news mail views

--- db/migrate/20180423145159_create_statistics.rb
+++ db/migrate/20180423145159_create_statistics.rb
@@ -14,12 +14,12 @@ class CreateStatistics < ActiveRecord::Migration[5.1]
     Statistic.create(news_type:"us_sports" , sended_times:"0" , current_daily_subs: "0", current_weekly_subs: "0")
     Statistic.create(news_type:"us_technology" , sended_times:"0" , current_daily_subs: "0", current_weekly_subs: "0")
 
-    Statistic.create(news_type:"ua_business" , sended_times:"0" , current_subs: "0")
-    Statistic.create(news_type:"ua_entertainment" , sended_times:"0" , current_subs: "0")
-    Statistic.create(news_type:"ua_health" , sended_times:"0" , current_subs: "0")
-    Statistic.create(news_type:"ua_science" , sended_times:"0" , current_subs: "0")
-    Statistic.create(news_type:"ua_sports" , sended_times:"0" , current_subs: "0")
-    Statistic.create(news_type:"ua_technology" , sended_times:"0" , current_subs: "0")
+    Statistic.create(news_type:"ua_business" , sended_times:"0" , current_daily_subs: "0", current_weekly_subs: "0")
+    Statistic.create(news_type:"ua_entertainment" , sended_times:"0" , current_daily_subs: "0", current_weekly_subs: "0")
+    Statistic.create(news_type:"ua_health" , sended_times:"0" , current_daily_subs: "0", current_weekly_subs: "0")
+    Statistic.create(news_type:"ua_science" , sended_times:"0" , current_daily_subs: "0", current_weekly_subs: "0")
+    Statistic.create(news_type:"ua_sports" , sended_times:"0" , current_daily_subs: "0", current_weekly_subs: "0")
+    Statistic.create(news_type:"ua_technology" , sended_times:"0" , current_daily_subs: "0", current_weekly_subs: "0")
     AdminUser.create(email: "admin@admin.com" , password: "admin")
   end
 end
