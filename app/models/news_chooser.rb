1e1570250e47848b73e8769fb91667242b5e07c3
ed1ff <edifei13@yandex.ua>
2018-05-01 21:40:30 +0300

    news_chooser tests done

--- app/models/news_chooser.rb
+++ app/models/news_chooser.rb
@@ -1,48 +1,43 @@
 class NewsChooser < ApplicationRecord
   belongs_to :user
 
-  
-
-  def self.news_types_changer(news_id , current_user)
+  def self.news_types_changer(news_id, current_user)
     @news_chooser = NewsChooser.where(user_id: current_user).last
     a = @news_chooser.news_types.each_char.to_a
 
-    if a[news_id] == '0'
-      a[news_id] = '1'
-    else
-      a[news_id] = '0'
-    end
+    a[news_id] = if a[news_id] == '0'
+                   '1'
+                 else
+                   '0'
+                 end
 
     b = a.join
-    @news_chooser.update!(:news_types =>  b)
+    @news_chooser.update!(news_types: b)
   end
-
-
 
   def self.send_daily_changer(current_user)
     @send_choose = NewsChooser.where(user_id: current_user).last
     a = @send_choose.send_daily
 
-    if a == 0
-      a = 1
-    else
-      a = 0
-    end
+    a = if a == 0
+          1
+        else
+          0
+        end
 
-    @send_choose.update!(:send_daily =>  a)
+    @send_choose.update!(send_daily: a)
   end
 
   def self.send_weekly_changer(current_user)
     @send_choose = NewsChooser.where(user_id: current_user).last
     a = @send_choose.send_weekly
 
-    if a == 0
-      a = 1
-    else
-      a = 0
-    end
+    a = if a == 0
+          1
+        else
+          0
+        end
 
-    @send_choose.update!(:send_weekly =>  a)
+    @send_choose.update!(send_weekly: a)
   end
-
 end
--- spec/models/news_chooser_spec.rb
+++ spec/models/news_chooser_spec.rb
@@ -1,4 +1,47 @@
-require "rails_helper"
+require 'rails_helper'
 RSpec.describe NewsChooser, type: :model do
-  
+  context 'model defs' do
+    let!(:user) { User.create(email: 'aome@some.some', password: 'somepass') }
+    let!(:id) { User.current = user  }
+    let!(:notification) { Notification.create(email: 'aome@some.some', user_id: user.id) }
+    let!(:news_chooser) { NewsChooser.where(user_id: user.id).last }
+    # news_types_changer---------------------------------------------------
+    it 'perform news_types_changer 1 time to get 1' do
+      NewsChooser.news_types_changer(0, user.id)
+      news_chooser = NewsChooser.where(user_id: user.id).last
+      ec = news_chooser.news_types.each_char.to_a
+      expect(ec[0]).to eq('1')
+    end
+    it 'perform news_types_changer 2 times to get 0' do
+      NewsChooser.news_types_changer(0, user.id)
+      NewsChooser.news_types_changer(0, user.id)
+      news_chooser = NewsChooser.where(user_id: user.id).last
+      ec = news_chooser.news_types.each_char.to_a
+      expect(ec[0]).to eq('0')
+    end
+    # send_daily_changer---------------------------------------------------
+    it 'perform send_daily_changer 1 time to get 1' do
+      NewsChooser.send_daily_changer(user.id)
+      news_chooser = NewsChooser.where(user_id: user.id).last
+      expect(news_chooser.send_daily).to eq(1)
+    end
+    it 'perform send_daily_changer 2 time to get 0' do
+      NewsChooser.send_daily_changer(user.id)
+      NewsChooser.send_daily_changer(user.id)
+      news_chooser = NewsChooser.where(user_id: user.id).last
+      expect(news_chooser.send_daily).to eq(0)
+    end
+    # send_weekly_changer---------------------------------------------------
+    it 'perform send_weekly_changer 1 time to get 1' do
+      NewsChooser.send_weekly_changer(user.id)
+      news_chooser = NewsChooser.where(user_id: user.id).last
+      expect(news_chooser.send_weekly).to eq(1)
+    end
+    it 'perform send_weekly_changer 2 time to get 0' do
+      NewsChooser.send_weekly_changer(user.id)
+      NewsChooser.send_weekly_changer(user.id)
+      news_chooser = NewsChooser.where(user_id: user.id).last
+      expect(news_chooser.send_weekly).to eq(0)
+    end
+  end
 end
